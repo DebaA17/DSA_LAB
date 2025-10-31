593872864152d2543028eeff083f6d49b0fef631
DEBASIS <185465407+DebaA17@users.noreply.github.com>
2025-10-31 12:17:14 +0530

    Linear search

---
+++ searching and sorting/linear.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+// Function to perform linear search
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;  
+        }
+    }
+    return -1;  
+}
+
+int main() {
+    int size, target;
+
+    
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    
+    int arr[size];
+
+    
+    printf("Enter %d elements for the array:\n", size);
+    for (int i = 0; i < size; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Enter the element to search: ");
+    scanf("%d", &target);
+
+    
+    int index = linearSearch(arr, size, target);
+
+    
+    if (index != -1) {
+        printf("Element %d found at index %d.\n", target, index);
+    } else {
+        printf("Element %d not found in the array.\n", target);
+    }
+
+    return 0;
+}
+
